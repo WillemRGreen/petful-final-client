af206ad129829fb7ffe03b0bdd53d5dd20be6a97
Willem Green <willberforce15@gmail.com>
2021-01-30 19:25:41 -0600

    final notes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,20 +10,20 @@ export default class Home extends React.Component {
                     <div>
                         <img className='home-pet-pic' src={'https://images.pexels.com/photos/33053/dog-young-dog-small-dog-maltese.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt='cat'></img>
                     </div>
-                <div className='home'>
-                <p>
-                {' '}
-                FIFO is a pet adoption agency that takes a 'first-in-first-out'
-                approach to adoption.
-                </p>{' '}
-                <p>
-                {' '}
-                The first animal that gets admitted to our shelter is the first one
-                that gets adopted.
-                </p>
-                <div>
-                <button className='button' onClick={() => this.props.history.push('/adopt')}>Adopt Now!</button>
-                </div>
+                    <div className='home'>
+                    <p>
+                    {' '}
+                    FIFO is a pet adoption agency that takes a 'first-in-first-out'
+                    approach to adoption.
+                    </p>{' '}
+                    <p>
+                    {' '}
+                    The first animal that gets admitted to our shelter is the first one
+                    that gets adopted.
+                    </p>
+                    <div>
+                    <button className='button' onClick={() => this.props.history.push('/adopt')}>Adopt Now!</button>
+                    </div>
                 </div>
             </div>
         );
