65de0d5ca669cac1e3a7bb59795309fee805a1ce
Willem Green <willberforce15@gmail.com>
2021-01-30 18:57:55 -0600

    url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  //API_BASE_URL: 'https://tranquil-sands-16766.herokuapp.com',
-  API_BASE_URL: 'http://localhost:8000',
+  API_BASE_URL: 'https://tranquil-sands-16766.herokuapp.com',
+  //API_BASE_URL: 'http://localhost:8000',
 };
